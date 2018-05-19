7b6b721b0105b21275e3367fb817c61a2e3548b6
suneetha.kori <suneetha.kori@gmail.com>
2018-05-18 23:40:15 -0500

    fixed for code review comments

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,9 +1,9 @@
 
 //for loop to iterate the value of an array
 const carsInReverse= ['Honda', 'Chevy', 'Acura', 'Toyota', 'Mazada','Subaru', 'Nissan', 'Ford'];
-carsInReverse.forEach((item) => {
-  console.log(item);
-});
+for(let i=0; i<carsInReverse.length; i++ ){
+  console.log(carsInReverse[i]);
+}
 
 //for in loop to print each key of an object
 let persons = {
@@ -40,3 +40,16 @@ do{
   console.log(num);
   num++
 }while( num < 1000);
+
+// When is a for loop better than a while loop?
+//When the number of iterations is known, for loop is better
+
+// How is the readability of the code affected?
+//The start and ending value of the loop is visible at the top in the for loop.
+
+// What is the difference between a for loop and a for...in loop?
+//for loop works for a fixed number of iterations. for ...in is used to iterate through the properties of an object.
+
+// What is the difference between a while loop and a do...while loop?
+//In a while loop condition is checked first and the body id executed if it is true.
+//In a do while loop the body is executed first and then the condition is checked. So it will execute at least once.
